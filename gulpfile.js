9223f5c9785ae749ad8447b88aa8474b4b3ab14d
Brandon McGowan <brandon.lee.mcgowan@live.com>
2019-04-11 01:26:40 -0700

    Gulp and PostCSS

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'), 
+nested = require('postcss-nested') ; 
+
+gulp.task('default', function(){
+    console.log('Hooray - you created a Gulp task.') ; 
+}) ; 
+
+gulp.task('html', function(){
+    console.log('Image something useful being done to your HTML here.') ; 
+});
+
+gulp.task('styles', function(){
+    console.log('is this working') ; 
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars]))
+    .pipe(postcss([nested]))
+    .pipe(postcss([autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'))
+});
+
+gulp.task('watch', function(){
+
+    watch('./app/index.html', function(){
+        gulp.start('html') ; 
+    }) ; 
+
+    watch('./app/assets/styles/**.css', function(){
+        gulp.start('styles')
+    })
+
+}) ; 
